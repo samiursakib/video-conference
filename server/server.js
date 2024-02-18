adeefee9dcdfb6953fd31abf4eccd8cef98b0822
samiursakib <rafisamiur@gmail.com>
2024-02-19 02:17:10 +0600

    cors in server updated again

--- server/server.js
+++ server/server.js
@@ -12,11 +12,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      'http://localhost:3000',
-      'https://video-conference-client.vercel.app',
-    ],
-    methods: ['GET', 'POST'],
+    origin: '*',
+    // methods: ['GET', 'POST'],
   },
 });
 const peerServer = new PeerServer({
